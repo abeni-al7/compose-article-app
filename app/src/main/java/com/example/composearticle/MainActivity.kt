a54c14bf66d0e1c9d2871cf37d5c47faf171727c
abeni-al7 <abenezeralebachew3@gmail.com>
2025-02-23 15:50:09 +0300

    Complete an Article UI using compose

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +32,32 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeArticleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Column(modifier = Modifier.safeDrawingPadding()) {
+                        BackgroundImage(
+                            painter = painterResource(R.drawable.bg_compose_background)
+                        )
+                        TitleText(
+                            text = stringResource(R.string.title_text)
+                        )
+                        ParagraphText(
+                            text = stringResource(R.string.paragraph_1),
+                            padding = PaddingValues(
+                                start = 16.dp,
+                                end = 16.dp
+
+                            )
+                        )
+                        ParagraphText(
+                            text = stringResource(R.string.paragraph_2),
+                            padding = PaddingValues(
+                                start = 16.dp,
+                                end = 16.dp,
+                                top = 16.dp,
+                                bottom = 16.dp
+                            )
+                        )
+                    }
                 }
             }
         }
@@ -31,10 +65,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun BackgroundImage(painter: Painter, modifier: Modifier = Modifier) {
+    Image(
+        painter = painter,
+        contentDescription = null,
+        modifier = modifier.fillMaxWidth()
+    )
+}
+
+@Composable
+fun TitleText(text: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = text,
+        fontSize = 24.sp,
+        modifier = modifier.padding(16.dp)
+    )
+}
+
+@Composable
+fun ParagraphText(text: String, padding: PaddingValues, modifier: Modifier = Modifier) {
+    Text(
+        text = text,
         modifier = modifier
+            .padding(padding),
+        textAlign = TextAlign.Justify
     )
 }
 
@@ -42,6 +96,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ComposeArticleTheme {
-        Greeting("Android")
+
     }
 }
